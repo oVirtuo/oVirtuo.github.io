4e55d48cd41c9371fd96335fcf68142150567058
Vitor Cunha Barboza <josevitor.barboza@gmail.com>
2024-10-26 15:47:58 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+],
+define: { 'process.env': {} },
+resolve: {
+    alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: [
+        '.js',
+        '.json',
+        '.jsx',
+        '.mjs',
+        '.ts',
+        '.tsx',
+        '.vue',
+    ],
+},
+server: {
+    port: 3000,
+    open: true,
+    cors: true,
+},
+base: './',
 })
+
+
